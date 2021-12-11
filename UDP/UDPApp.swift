f20d52f0779e67592fb95f87857c4b98451a8b80
Ali Aghamirbabaei <Me@AliAghamirbabaei.com>
2021-12-11 13:23:55 +0330

    Initial Commit

---
+++ UDP/UDPApp.swift
@@ -0,0 +1,17 @@
+//
+//  UDPApp.swift
+//  UDP
+//
+//  Created by Ali Aghamirbabaei on 12/11/21.
+//
+
+import SwiftUI
+
+@main
+struct UDPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
